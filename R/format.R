adae6dd24f108729b4ed21e83f1e09a338f78cae
Didion, John (NIH/NHGRI) [F] <john.didion@nih.gov>
2015-05-12 12:10:21 -0400

    initial checkin

---
+++ R/format.R
@@ -0,0 +1,57 @@
+#' Format a vector of values as a comma-delimited list.
+#'
+#' @param v vector of values
+#' @param fn function to apply to `v` (or NULL to use `v` as-is)
+#' @param ... additional arguments to `fn`
+#' @param oxford use an oxford comma
+fmt.list <- function(v, fn=NULL, ..., oxford=TRUE) {
+    if (!is.null(fn)) {
+        v <- sapply(v, fn, ...)
+    }
+    if (length(v) == 1) {
+        v
+    }
+    else if (length(v) == 2) {
+        paste(v, collapse=" and ")
+    }
+    else {
+        s <- paste(v[-length(v)], collapse=", ")
+        if (oxford) {
+            s <- paste0(s, ",")
+        }
+        paste0(s, " and ", v[length(v)])
+    }
+}
+
+#' Format numeric values as percentages.
+#'
+#' @param v vector of numeric values
+#' @param digits number of digits to print
+fmt.pct <- function(v, digits=0) {
+    if (length(v) == 1) {
+        paste0(round(v*100, digits), "%")
+    }
+    else {
+        fmt.list(v, fmt.pct, digits=digits)
+    }
+}
+
+#' Format a vector as a range of values.
+#'
+#' @param v numeric vector
+#' @param digits number of digits to print (if values are not integers)
+fmt.range <- function(v, digits=2) {
+    if (all(is.integer(v))) {
+        digits <- 0
+    }
+    paste(round(range(v), digits), collapse="-")
+}
+
+#' Format a logical vector as a sum (total number of true values) and percentage
+#' (fraction of all values that are true).
+#'
+#' @param v logical vector
+#' @param digits number of digits to print for percentage
+fmt.true <- function(v, digits=2) {
+    paste0(sum(v), " (", round((sum(v) / length(v)) * 100, 2), "%)")
+}
---
+++ R/references.R
@@ -0,0 +1,106 @@
+libfary(kfigr)
+library(tools)
+
+#' Print a section label.
+#'
+#' @param label the section label
+section.label <- local({
+    function(label) {
+        sprintf('# <a name="%s"/> %s', label, label)
+    }    
+})
+
+#' Print a reference to a section (using the section label).
+#'
+#' @param label the section label
+section.ref <- local({
+    function(label) {
+        sprintf('[%s](#%s)', label, label)
+    }    
+})
+
+#' Print an anchor for the current page that can be referenced
+#' with `page.ref`.
+#'
+#' @param print.num whether to print the page number.
+page.anchor <- local({
+    function(print.num=T) {
+        if (print.num) {
+            sprintf('\n-----\nPage-%i\n<a name="Page-%i"/>\n', pgcount(inc=F), pgcount())
+        } else {
+            sprintf('\n<a name="Page-%i"/>\n', pgcount())
+        }
+    }
+})
+
+#' Print a reference to a page (using the page number).
+#'
+#' @param n the page number
+page.ref <- local({
+    function(n) {
+        sprintf('[Page-%i](#Page-%i)', n, n)
+    }
+})
+
+#' Print the current page count.
+#'
+#' @param inc if TRUE, the current page number will be incremented. 
+#' This can be used to number pages.
+page.count <- local({
+    pg <- 0
+    function(inc=T) {
+        if (inc) { pg <<- pg + 1 }
+        pg
+    }
+})
+
+# Add a hook to echo the chunk name
+knit_hooks$set(echo.label=function(before, options, envir) {
+    if (before) {
+        sprintf('> %s', options$label)
+    }
+})
+
+# Add a hook fo place a page break after the chunk
+knit_hooks$set(pgbreak=function(before, options, envir) {
+    if (!before) {
+        pganchor();
+    }
+})
+
+## Extensions to kfigr
+
+#' Wrap figr with two improvements:
+#' 1. Prefixes printed with first character upper case (optionally)
+#' 2. Ability to format a list of references
+#'
+#' @param labels character vector of labels to reference.
+#' @param types character vector of label types; must either be of length 1
+#' (meaning all labels are of the same type), or of the same length as `labels`.
+#' @param group logical; if true, labels of the same type will be grouped together.
+figr.ref <- function(labels, types, group=TRUE, first.char.upper=T) {
+    stopifnot(length(types) %in% c(1, length(labels)))
+    .format.refs <- function(labels, type) {
+        refs <- sapply(labels, figr, prefix=F, link=T, type=type)
+        prefix <- type
+        if (first.char.upper) {
+            prefix <- paste0(toupper(substr(prefix,1,1)), substring(type,2))
+        }
+        if (length(refs) > 1) {
+            prefix <- paste0(prefix, "s")
+            refs <- paste(paste(refs[-length(refs)], collapse=", "), refs[length(refs)], sep=" and ")
+        }
+        paste(prefix, refs)
+    }
+    if (length(types) == 1) {
+        .format.refs(labels, types[1])
+    }
+    else if (group) {
+        groups <- tapply(labels, types, .format.refs)
+        paste(groups, collapse="; ")
+    }
+    else {
+        refs <- sapply(1:length(labels), function(i) .format.refs(labels[i], types[i])
+        paste(paste(refs[-length(refs)], collapse=", "), refs[length(refs)], sep=" and ")
+    }    
+}
