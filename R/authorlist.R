e6329628d2534b2e5ad8212716737f97f078b0be
John Didion <github@didion.net>
2015-08-10 11:29:17 -0400

    re-write to use data.table

--- R/authorlist.R
+++ R/authorlist.R
@@ -1,77 +1,88 @@
 #' Format one or more names as a set of initials
-format.initial <- function(x, initial.sep='') {
-    parts <- strsplit(x, ' ', fixed=T)[[1]]
-    initials <- toupper(sapply(parts, substr, 1, 1))
-    paste(initials, sep=initial.sep)
+#' @param initial.sep string separating consecutive initials
+format.initial <- function(initial.sep='') {
+    function(x) {
+        parts <- strsplit(x, ' ', fixed=T)[[1]]
+        initials <- toupper(sapply(parts, substr, 1, 1))
+        paste(initials, sep=initial.sep)
+    }
 }
 
 #' Format an author as a full name (First MI Last)
 #' @param suffix.sep string separating the suffixe (e.g. Jr.) from the last name
-#' @param initial.sep string separating consecutive initials
-format.full.name <- function(fields, suffix.sep=' ', initial.sep='') {
-    author.str <- fields$First
-    if (!is.na(fields$Middle)) {
-        middle <- format.initial(fields$Middle, initial.sep)
-        author.str <- paste(author.str, middle)
+format.full.name <- function(suffix.sep=' ') {
+    function(first, middle, last, suffix, initial.format) {
+        author.str <- first
+        if (!is.na(middle)) {
+            middle <- initial.format(middle)
+            author.str <- paste(author.str, middle)
+        }
+        author.str <- paste(author.str, last)
+        if (!is.na(suffix)) {
+            author.str <- paste0(author.str, suffix.sep, suffix)
+        }
+        author.str
     }
-    author.str <- paste(author.str, fields$Last)
-    if (!is.na(fields$Suffix)) {
-        author.str <- paste(paste0(author.str, suffix.sep), fields$Suffix)
-    }
-    author.str
 }
 
 # Format an author as initials followed by last name (FM Last)
 #' @param suffix.sep string separating the suffixe (e.g. Jr.) from the last name
-#' @param initial.sep string separating consecutive initials
-format.initial.first <- function(fields, suffix.sep=' ', initial.sep='') {
-    init.str <- fmt.initial(fields$First, initial.sep)
-    if (!is.na(fields$Middle)) {
-        middle <- format.initial(fields$Middle, initial.sep)
-        init.str <- paste0(init.str, middle)
+format.initial.first <- function(suffix.sep=' ') {
+    function(first, middle, last, suffix, initial.format) {
+        init.str <- fmt.initial(first)
+        if (!is.na(middle)) {
+            middle <- initial.format(middle)
+            init.str <- paste0(init.str, middle)
+        }
+        author.str <- paste(init.str, last)
+        if (!is.na(fields$Suffix)) {
+            author.str <- paste0(author.str, suffix.sep, suffix)
+        }
+        author.str
     }
-    author.str <- paste(init.str, fields$Last)
-    if (!is.na(fields$Suffix)) {
-        author.str <- paste(paste0(author.str, suffix.sep), fields$Suffix)
-    }
-    author.str
 }
 
 #' Format an author as a last name followed by initials (Last FM)
 #' @param suffix.sep string separating the suffixe (e.g. Jr.) from the last name
-#' @param initial.sep string separating consecutive initials
 #' @parma first.last.sep when `naming == 'initial_after'`, the separtor between the
 #' last name and the initials
-format.initial.last <- function(fields, suffix.sep=' ', initial.sep='', first.last.sep=', ') {
-    init.str <- fmt.initial(fields$First, initial.sep)
-    if (!is.na(fields$Middle)) {
-        middle <- format.initial(fields$Middle, initial.sep)
-        init.str <- paste0(init.str, middle)
+format.initial.last <- function(suffix.sep=' ', first.last.sep=', ') {
+    function(first, middle, last, suffix, initial.format) {
+        init.str <- fmt.initial(first)
+        if (!is.na(middle)) {
+            middle <- initial.format(middle)
+            init.str <- paste0(init.str, middle)
+        }
+        author.str <- last
+        if (!is.na(suffix)) {
+            author.str <- paste0(author.str, suffix.sep, suffix)
+        }
+        author.str <- paste0(author.str, first.last.sep, init.str)
+        author.str
     }
-    author.str <- fields$Last
-    if (!is.na(fields$Suffix)) {
-        author.str <- paste(paste0(author.str, suffix.sep), fields$Suffix)
-    }
-    author.str <- paste0(author.str, first.last.sep, init.str)
-    author.str
 }
 
 #' Format an affiliation.
 #' @param label.sep separator between afiliation label and string
-format.affiliation <- function(label, fields, label.sep=". ", part.sep=", ") {
-    fields <- fields[!is.na(fields)]
-    paste(label, paste(fields, collapse=part.sep), sep=label.sep)
+format.affiliation <- function(label.sep=". ", part.sep=", ") {
+    function(label, ...) {
+        fields <- list(...)
+        fields <- fields[!is.na(fields)]
+        paste(label, paste(fields, collapse=part.sep), sep=label.sep)
+    }
 }
 
 #' Format a list of affiliation references.
 #' @param affil.sep separator between multiple affiliations after an author name
 #' @param affil.wrap function to wrap the list of an author's affiliations
-format.affiliation.refs <- function(labels, affil.sep=',', affil.wrap=NULL) {
-    affil.str <- paste(labels, collapse=affil.sep)
-    if (!is.null(affil.wrap)) {
-        affil.str <- affil.wrap(affil.str)
+format.affiliation.refs <- function(affil.sep=',', affil.wrap=NULL) {
+    function(labels) {
+        affil.str <- paste(labels, collapse=affil.sep)
+        if (!is.null(affil.wrap)) {
+            affil.str <- affil.wrap(affil.str)
+        }
+        affil.str
     }
-    affil.str
 }
 
 # Wrap function for an affiliation list to make it superscript
@@ -93,13 +104,10 @@ markup.ss <- function(s) paste0("<sup>", s, "</sup>")
 #' @param authors path to Excel spreadsheet, or data.frame
 #' @param affiliations NULL if `names` is a path, otherwise a data.frame
 #' @param author.format function to format author names
-#' @param author.format.args optional arguments to author name formatting function
 #' @param affil.labeling function for converting an integer index into the label for the
 #' affiliation
 #' @param affil.format function to format an affiliation
-#' @param affil.format.args optional arguments to affiliation formatting function
 #' @param affil.refs.format function to format list of affiliation references
-#' @param affil.refs.format.args optional arguments to affilation reference formatting function
 #' @param equal.symbols vector of symbols to use to denote equal contributions
 #' @param author.affil.ref.sep separator between author name and list of affiliation reference
 #' @param author.sep separator to use when concatenating authors
@@ -120,73 +128,72 @@ markup.ss <- function(s) paste0("<sup>", s, "</sup>")
 #' author.str: the formatted list of authors as a string
 #' affiliation.str: the formatted list of affiliations of a string
 format.authors <- function(authors, affiliations=NULL, 
-                           author.format=format.full.name, author.format.args=NULL,
+                           author.format=format.full.name(),
+                           initial.format=format.initial(),
                            affil.labeling=identity, 
-                           affil.format=format.affiliation, affil.format.args=NULL,
-                           affil.refs.format=format.affiliation.refs, affil.refs.format.args=NULL,
-                           equal.symbols='*', author.affil.ref.sep=' ', author.sep=", ", affil.sep="\n", 
-                           outfile=NULL) {
+                           affil.format=format.affiliation(), 
+                           affil.refs.format=format.affiliation.refs(), 
+                           equal.symbols='*', author.affil.ref.sep='') {
     if (is.character(authors)) {
         infile <- authors
-        authors <- read_excel(infile)
+        authors <- data.table(read_excel(infile))
         if (is.null(affiliations)) {
-            affiliations <- read_excel(infile, sheet=2)
+            affiliations <- data.table(read_excel(infile, sheet=2))
         }
     }
     else if (is.null(authors) || is.null(affiliations)) {
         stop("authors and affiliations required")
     }
 
-    id.order <- unique(unlist(strsplit(authors$Affiliations, '')))
+    authors[,AffilList := strsplit(Affiliations, "")]
+    authors[,FormattedName := author.format(
+        First, Middle, Last, Suffix, initial.format),
+        by=1:nrow(authors)]
+    
+    id.order <- unique(unlist(authors$AffilList))
     m <- match(id.order, affiliations$ID)
     if (any(is.na(m))) {
         stop(paste("Missing affiliations", paste(id.order[is.na(m)], sep=",")))
     }
+    
     affiliations <- affiliations[m,]
+    affiliations[,Label := affil.labeling(.I), by=1:nrow(affiliations)]
+    affiliations[,Formatted := affil.format(
+        Label, Department, Institution, City, Locality, Country), 
+        by=1:nrow(affiliations)]
     
-    affil.list <- lapply(1:nrow(affiliations), function(i) {
-        label <- affil.labeling(i)
-        fields <- affiliations[i, c('Department','Institution','City','Locality','Country')]
-        affil.str <- do.call(affil.format, c(list(label, fields), affil.format.args))
-        list(ID=affiliations[i,'ID'], label=label, affiliation=affil.str)
-    })
-    names(affil.list) <- id.order
-    
-    affil.ref <- lapply(authors$Affiliations, function(x) {
-        ids <- strsplit(x, '')[[1]]
-        labels <- unlist(lapply(affil.list[ids], "[[", "label"))
-        affil.str <- do.call(affil.refs.format, c(list(labels), affil.refs.format.args))
-        list(ids=ids, affil.str=affil.str)
-    })
-    
-    name.str <- apply(authors, 1, function(x) 
-        do.call(author.format, c(list(x), author.format.args)))
-    author.str <- paste(name.str, unlist(lapply(affil.ref, "[[", "affil.str")), 
-                        sep=author.affil.ref.sep)
-    
-    eq <- unique(authors$Equal[!is.na(authors$Equal)])
-    eq.sym <- rep(NA, length(author.str))
-    stopifnot(length(equal.symbols) >= length(eq))
-    
-    for (i in 1:length(eq)) {
-        symbol <- equal.symbols[i]
-        w <- which(authors$Equal == eq[i])
-        eq.sym[w] <- symbol
-        author.str[w] <- paste0(author.str[w], symbol)
+    .format.affil.list <- function(ids) {
+        labels <- affiliations[ID %in% ids, Label]
+        affil.refs.format(labels)
     }
+    authors[,AffilRef := .format.affil.list(AffilList[[1]]), by=1:nrow(authors)]
     
-    author.list <- lapply(1:length(author.str), function(i) list(
-        author=author.str[i], name=name.str[i],
-        affil.ref=affil.ref[i], equal.sym=eq.sym[i]
-    ))
+    eq.ids <- unique(authors[!is.na(Equal),Equal])
+    if (length(equal.symbols) > length(eq.ids)) {
+        warning("Not enough equal symbols; recycling")
+        equal.symbols <- rep_len(equal.symbols, length(eq.ids))
+    }
+    authors$Equal <- factor(authors$Equal, levels=eq.ids)
+    authors[!is.na(Equal), EqualSym := equal.symbols[as.integer(Equal)], by=which(!is.na(Equal))]
     
-    retval <- list(authors=author.list, affiliations=affil.list, 
-         author.str=paste(lapply(author.list, "[[", "author"), collapse=author.sep),
-         affiliation.str=paste(lapply(affil.list, "[[", "affiliation"), collapse=affil.sep))
+    authors[,Formatted := paste(FormattedName, AffilRef, sep=author.affil.ref.sep)]
+    authors[!is.na(Equal), Formatted := paste0(Formatted, EqualSym)]
     
+    list(authors=authors, affiliations=affiliations)
+}
+
+.format.list <- function(dt, sep, outfile) {
+    str <- paste(dt[,Formatted], collapse=sep)
     if (!is.null(outfile)) {
-        writeLines(c(retval$author.str, retval$affiliation.str), outfile)
+        writeLines(str, outfile)
     }
-    
-    invisible(retval)
+    str
+}
+
+format.author.list <- function(authors, sep=", ", outfile=NULL) {
+    .format.list(authors, sep, outfile)
+}
+
+format.affiliation.list <- function(affiliations, sep="\n", outfile=NULL) {
+    .format.list(affiliations, sep, outfile)
 }
